d2dc7c6dc06863bca80b786538e16c86b9edbbe1
rezatn0934 <rezatn0934@gmail.com>
2023-09-15 16:10:07 +0330

    chore: define Brand model

--- store/models.py
+++ store/models.py
@@ -9,3 +9,10 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
+class Brand(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
