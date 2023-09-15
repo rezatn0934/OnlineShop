7b715c37572ff89d350d8dd4d4135568e1c913c2
rezatn0934 <rezatn0934@gmail.com>
2023-09-15 16:44:04 +0330

    chore: define UserLogin

--- accounts/views.py
+++ accounts/views.py
@@ -42,3 +42,45 @@ class UserRegister(APIView):
             return Response(ser_data.data, status=status.HTTP_201_CREATED)
         return Response(ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+class UserLogin(APIView):
+    """
+    Log in an existing user and issue access and refresh tokens.
+
+    Endpoint: POST /api/auth/login/
+    Permission: AllowAny (open to all users)
+
+    This view allows users to log in by providing valid credentials (username/email and password).
+    It issues an access token and a refresh token upon successful login.
+
+    Args:
+        request (HttpRequest): The HTTP request object containing user login data.
+
+    Returns:
+        Response: A JSON response containing access and refresh tokens along with appropriate status codes.
+    """
+
+    permission_classes = (AllowAny,)
+    serializer_class = UserLoginSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user_identifier = serializer.validated_data.get('user_identifier')
+        password = serializer.validated_data.get('password')
+        user = AuthBackend().authenticate(request, username=user_identifier, password=password)
+        if user is None:
+            return Response({'message': 'Invalid Credentials'}, status=status.HTTP_400_BAD_REQUEST)
+
+        jti = jti_maker(request, user.id)
+        access_token = generate_access_token(user.id, jti)
+        refresh_token = generate_refresh_token(user.id, jti, settings.REDIS_CACHE_TTL)
+
+        cache.set(jti, 0)
+
+        data = {
+            "access": access_token,
+            "refresh": refresh_token,
+        }
+        return Response(data, status=status.HTTP_201_CREATED)
+
