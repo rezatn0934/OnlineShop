eb1c3f126533228ef50cd3c7d36bad8716a45663
rezatn0934 <rezatn0934@gmail.com>
2023-09-15 16:04:53 +0330

    chore: define customer dunder str

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.core.validators import EmailValidator
+from django.conf import settings
 from django.utils.translation import gettext_lazy as _
 from django.db import models
 
@@ -26,3 +27,24 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.username
+
+
+class Customer(models.Model):
+    MEMBERSHIP_FREE = 'F'
+    MEMBERSHIP_VIP = 'V'
+
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_FREE, 'Free'),
+        (MEMBERSHIP_VIP, 'Vip'),
+    ]
+
+    birth_date = models.DateField(null=True, blank=True)
+    membership = models.CharField(
+        max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_FREE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user.first_name} {self.user.last_name}'
+
+    class Meta:
+        ordering = ['user__first_name', 'user__last_name']
