8a2541d1b202a88d6c5635063ecb0f6a8c9694c1
rezatn0934 <rezatn0934@gmail.com>
2023-09-15 16:43:46 +0330

    chore: define UserRegister

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
 
 # Create your views here.
+import jwt
+from django.conf import settings
+from django.core.cache import cache
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .authentication import AuthBackend, JWTAuthentication
+from .utils import generate_access_token, generate_refresh_token, jti_maker
+from .serilizers import UserRegisterSerializer, UserLoginSerializer
+
+access_token_lifetime = settings.JWT["ACCESS_TOKEN_LIFETIME"].total_seconds()
+refresh_token_lifetime = settings.JWT["REFRESH_TOKEN_LIFETIME"].total_seconds()
+
+
+class UserRegister(APIView):
+    """
+    Register a new user.
+
+    Endpoint: POST /api/auth/register/
+    Permission: AllowAny (open to all users)
+
+    This view allows users to register by providing valid user data.
+
+    Args:
+        request (HttpRequest): The HTTP request object containing user registration data.
+
+    Returns:
+        Response: A JSON response indicating success or failure along with appropriate status codes.
+    """
+
+    permission_classes = (AllowAny,)
+
+    def post(self, request):
+        ser_data = UserRegisterSerializer(data=request.POST)
+        if ser_data.is_valid():
+            ser_data.create(ser_data.validated_data)
+            return Response(ser_data.data, status=status.HTTP_201_CREATED)
+        return Response(ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
+
