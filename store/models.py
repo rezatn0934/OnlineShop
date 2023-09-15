c160eb0706b044e099bc851798feaa657931f0a3
rezatn0934 <rezatn0934@gmail.com>
2023-09-15 16:09:48 +0330

    chore: define Category model

--- store/models.py
+++ store/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    parent = models.ForeignKey('self', on_delete=models.SET_NULL, blank=True, null=True, related_name='categories')
+
+    def __str__(self):
+        return self.name
+
