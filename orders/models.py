51d3321c0342d8c66628852d3bb79c59d09e2aa8
rezatn0934 <rezatn0934@gmail.com>
2023-09-15 16:15:11 +0330

    chore: define Order model

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from accounts.models import Customer
 # Create your models here.
+
+
+class Order(models.Model):
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+    PAYMENT_STATUS_CHOICES = [
+        (PAYMENT_STATUS_PENDING, 'Pending'),
+        (PAYMENT_STATUS_COMPLETE, 'Paid'),
+        (PAYMENT_STATUS_FAILED, 'Failed')
+    ]
+
+    date = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(
+        max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"Order{self.id}"
