feb1e2335b9a76feeafced0b7e35947701c4bdcc
rezatn0934 <rezatn0934@gmail.com>
2023-09-21 11:02:40 +0330

    refactor: modify UserSerializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -25,8 +25,7 @@ class UserRegisterSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser',
-                  'date_joined', 'last_modify']
+        fields = ['username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser']
 
 
 class UserLoginSerializer(serializers.Serializer):
