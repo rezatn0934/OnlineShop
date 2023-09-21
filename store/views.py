27def5b3bd82650ce47e92dba6ed3a5929ed327d
rezatn0934 <rezatn0934@gmail.com>
2023-09-21 10:58:26 +0330

    chore: define ProductListView

--- store/views.py
+++ store/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 
 # Create your views here.
+
+from .models import Product
+from .serializers import ProductSerializer
+from .filters import ProductFilter
+
+
+class ProductListView(ListAPIView):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_class = ProductFilter
+    search_fields = ['name', 'description']
+    ordering_fields = ['name', 'description']
+
+    def get_serializer_context(self):
+        return {'request': self.request}
