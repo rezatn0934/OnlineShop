3ba57c4657b4f6114d7cb0ba1a8109c3de03cac4
rezatn0934 <rezatn0934@gmail.com>
2023-09-22 09:32:29 +0330

    chore: define validate method in CartItemSerializer

--- orders/serializers.py
+++ orders/serializers.py
@@ -7,3 +7,10 @@ class CartItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = CartItem
         fields = ['product', 'quantity']
+
+    def validate(self, attrs):
+        product = Product.objects.filter(pk=attrs['product'])
+        quantity = attrs['quantity']
+        if product.quantity < quantity:
+            raise serializers.ValidationError('Product quantity is less than what you want!')
+        return attrs
