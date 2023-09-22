4a6560d0ad0780c4b0e3eadabe0de841ce733722
rezatn0934 <rezatn0934@gmail.com>
2023-09-22 09:26:22 +0330

    chore: define cart item view set

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+
+from accounts.authentication import JWTAuthentication
+from accounts.permisions import UserIsOwner
+from orders.models import CartItem
+from orders.serializers import CartItemSerializer
+
 
 # Create your views here.
+
+
+class CartItemViewSet(ModelViewSet):
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated, UserIsOwner)
+    queryset = CartItem.objects.all()
+    serializer_class = CartItemSerializer
+
